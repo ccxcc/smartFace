2efde5ed2d4ae33d4e87ccdf024ea6b99bb7d2a8
ccxcc <celiaccx@hotmail.com>
2017-08-10 17:51:45 -0700

    base

---
+++ app/src/main/java/SmartFace/Constants.java
@@ -0,0 +1,17 @@
+package SmartFace;
+
+/**
+ * Created by goose on 8/10/17.
+ */
+
+public interface Constants {
+    int POSE_U_L = 0;
+    int POSE_U = 1;
+    int POSE_U_R = 2;
+    int POSE_C_L = 3;
+    int POSE_C = 4;
+    int POSE_C_R = 5;
+    int POSE_D_R = 6;
+    int POSE_D = 7;
+    int POSE_D_L = 8;
+}
--- app/src/main/java/SmartFace/SmartFace.java
+++ app/src/main/java/SmartFace/SmartFace.java
@@ -9,7 +9,6 @@ public class SmartFace {
     //members
 
     //9
-
     HeadPose headPose;
     Face face
     FaceLandmark faceLandmark
