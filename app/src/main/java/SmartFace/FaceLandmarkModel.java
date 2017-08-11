b9080a88a337a8457038b81e37a81fcc32f282f0
XingyuGuUCSD <xingyuguseu@gmail.com>
2017-08-11 13:19:58 -0700

    Update FaceLandmarkModel.java

--- app/src/main/java/SmartFace/FaceLandmarkModel.java
+++ app/src/main/java/SmartFace/FaceLandmarkModel.java
@@ -1,13 +1,13 @@
 package SmartFace;
-
+import java.util.ArrayList;
 /**
  * Created by goose on 8/10/17.
  */
 
 public class FaceLandmarkModel {
-    public int[] model;
+    public ArrayList<Point2D> model;
 
-    q
+    
     public int size;
     public FaceLandmarkModel(){
 
