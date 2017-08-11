60e4e1bae589c8d2edb12fc91748f52fe55f2951
XingyuGuUCSD <xingyuguseu@gmail.com>
2017-08-11 13:03:05 -0700

    Update FaceLandmark.java

--- app/src/main/java/SmartFace/FaceLandmark.java
+++ app/src/main/java/SmartFace/FaceLandmark.java
@@ -1,8 +1,20 @@
 package SmartFace;
-
+import java.util.ArrayList;
 /**
  * Created by goose on 8/10/17.
  */
 
 public class FaceLandmark {
+	ImageSize mImageSize;
+	FaceLocation mFaceLocation;
+	ArrayList<Point2d> mLandmark;
+
+	public FaceLandmark(ImageSize _mImageSize, FaceLocation _mFaceLocation, ArrayList<Point2d> _mLandmark){
+		if(_mLandmark.size != 68){
+			throw new Error("The size of mLandmark is not 68");
+		}
+		mImageSize = _mImageSize;
+		mFaceLocation = _mFaceLocation;
+		mLandmark = _mLandmark;
+	}
 }
