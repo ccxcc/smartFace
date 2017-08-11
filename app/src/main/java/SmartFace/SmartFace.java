a4c8ca5ad6b94c0f03d31ffb8293479c324b99f0
ccxcc <celiaccx@hotmail.com>
2017-08-10 18:57:38 -0700

    base

--- app/src/main/java/SmartFace/SmartFace.java
+++ app/src/main/java/SmartFace/SmartFace.java
@@ -22,7 +22,7 @@ public class SmartFace {
     //objects to generate members
     FaceRecognizor faceRecognizor;
     FaceDetector faceDetector;
-    FaceDescriptor faceDescriptor;
+    public FaceDescriptorModel FaceDescriptor(){};
 
 
     //initializer
@@ -34,7 +34,7 @@ public class SmartFace {
 
 
 
-    {}
+
 
 
 
